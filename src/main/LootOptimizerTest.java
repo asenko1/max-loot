27e6678e175e2a540dd6439610533ee351562e5e
asenko1 <alex.senko598@gmail.com>
2017-12-11 19:04:20 -0800

    better test

--- src/main/LootOptimizerTest.java
+++ src/main/LootOptimizerTest.java
@@ -5,15 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LootOptimizerTest {
+	public int[] values;
+	public int[] weights;
+	public int capacity;
+	
 	@Test
-	public void getOptimalValuetest() {
+	public void getOptimalValueCorrectnessTest() {
 		LootOptimizer test = new LootOptimizer();
 		
 		//test correctness
 		double expected = 180;
-		int[] values = new int[]{60,100,120};
-		int[] weights = new int[]{20,50,30};
-		int capacity = 50;
+		values = new int[]{60,100,120};
+		weights = new int[]{20,50,30};
+		capacity = 50;
 		assertEquals(expected, test.getOptimalValue(capacity, values, weights), 0);
 		
 		expected = (double) 500 / 3;
@@ -21,8 +25,12 @@ public class LootOptimizerTest {
 		weights = new int[]{30};
 		capacity = 10;
 		assertEquals(expected, test.getOptimalValue(capacity, values, weights), 0);
-		
-		
+	
+	}
+	
+	@Test
+	public void getOptimalValueEmptyTest(){
+		LootOptimizer test = new LootOptimizer();
 		//test empty
 		values = new int[]{};
 		weights = new int[]{};
@@ -30,7 +38,11 @@ public class LootOptimizerTest {
 		assertEquals(0, test.getOptimalValue(capacity, values, weights), 0);
 		capacity = 1;
 		assertEquals(0, test.getOptimalValue(capacity, values, weights), 0);
-		
+	}
+	
+	@Test
+	public void getOptimalValueNullTest(){
+		LootOptimizer test = new LootOptimizer();
 		//test null
 		values = null;
 		weights = null;
